249a9cbfbdefaf6c646a41242b3b22e908fe9a29
ved-t <111210095+ved-t@users.noreply.github.com>
2025-06-16 18:01:45 +0530

    Update ProfileViewModel.kt

--- app/src/main/java/com/example/taskmanager/presentation/viewmodel/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/taskmanager/presentation/viewmodel/profile/ProfileViewModel.kt
@@ -1,11 +1,12 @@
 package com.example.taskmanager.presentation.viewmodel.profile
 
+import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel @Inject constructor(
 
-) {
+): ViewModel(){
 
 }
